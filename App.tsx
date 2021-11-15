5a909367adb756af80d54828828b5f27daa8b4b7
Jardel Sousa <jardeelsousa@hotmail.com>
2021-11-15 20:55:03 -0300

    Create global theme

--- App.tsx
+++ App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
 
-export default function App() {
+import theme from './src/global/styles/theme';
+import Dashboard from './src/screens/Dashboard';
+
+const App: React.FC  = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    <ThemeProvider theme={theme}>
+      <Dashboard/>
+    </ThemeProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App 
