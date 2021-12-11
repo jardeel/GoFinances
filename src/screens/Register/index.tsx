9ec45183dee723af5045f3664d81741e7399f754
Jardel Sousa <jardeelsousa@hotmail.com>
2021-12-11 17:30:08 -0300

    Create and styling Buttom component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Input }  from '../../components/Forms/Input';
 import { Button }  from '../../components/Forms/Button';
+import { TransactionTypeButton }  from '../../components/Forms/TransactionTypeButton';
 
 import { 
   Container,
   Header,
   Title,
   Form,
-  Fields
+  Fields,
+  TransactionsTypes
 } from './styles';
 
 export function Register(){
+  const [transactionType, setTransactionType] = useState('');
+
+  function handleTransactionsTypeSelect(type: 'up' | 'down'){
+    setTransactionType(type);
+  }
+
   return(
     <Container>
       <Header>  
-        <Title>Cadastro1</Title>  
+        <Title>Cadastro</Title>  
       </Header>
 
       <Form>
         <Fields>
           <Input placeholder="Nome" />
           <Input placeholder="Preço" />
+
+          <TransactionsTypes> 
+            <TransactionTypeButton 
+              type="up"
+              title="Income"
+              onPress={() => handleTransactionsTypeSelect('up')}
+              isActive={transactionType === 'up'}
+            />
+            <TransactionTypeButton 
+              type="down"
+              title="Outcome"
+              onPress={() => handleTransactionsTypeSelect('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionsTypes>
         </Fields>
 
         <Button title="Enviar"/>
