2c0796c52492a16d281dbc1ad64c380de6d319d5
Jardel Sousa <jardeelsousa@hotmail.com>
2021-12-12 08:41:10 -0300

    Styling inteface for select category

--- App.tsx
+++ App.tsx
@@ -11,8 +11,7 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +27,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content"/>
-      <Register />
+      <CategorySelect />
     </ThemeProvider>
   );
 }
